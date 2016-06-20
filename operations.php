e1d8570152b3b84d15cca2c0cdb20ab2f1156cea
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-06-20 21:08:57 +0300

    first task

--- operations.php
+++ operations.php
@@ -7,3 +7,5 @@ $resultMult = $numberFloat * 5;
 $resultDiv = $numberFloat / 2;
 $resultPlus = $numberInt + 20;
 $resultMinus = $numberInt - 5;
+
+// 1. 3 города. $kharkiv = 1500000; Нужно получить среднее кол-во житилей в городах.

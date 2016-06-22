a26347888aaf3573eda5c3b56236385ff3151def
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-06-22 19:49:17 +0300

    array examples

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<?php
+// обычный массив
+$someArray = array(
+	1, 
+	2, 
+	3
+);
+
+// ассоциативный массив
+$arrayAssoc = array(
+    'key1' => 'value1',
+    'key2' => 'value2'
+);
+
+// ассоциативный многомерный массив
+$house = array(
+	'people' => array(
+		array(
+			'name' => 'Somename1'
+		),
+		array(
+            'name' => 'Somename2' 
+		)
+	)
+);

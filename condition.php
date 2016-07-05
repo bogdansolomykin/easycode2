753905781c0eaa0e6fc88170bdb53923d98e7736
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-07-05 19:33:33 +0300

    condition example

---
+++ condition.php
@@ -0,0 +1,11 @@
+<?php
+$i = 4;
+if ($i < 2) { // false
+    echo $i . ' < 2';
+} elseif ($i < 3) { // false
+    echo $i . ' < 3';
+} elseif ($i < 4) { // false
+    echo $i . ' < 4';
+} else {
+    echo $i . ' is too big';
+}

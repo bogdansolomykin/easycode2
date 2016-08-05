2d9f3fae7358c38b058c333b12e2428f1fc67d0a
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-08-05 20:47:15 +0300

    work

--- 05.08/todo/cookie.php
+++ 05.08/todo/cookie.php
@@ -2,6 +2,7 @@
 function saveData($header, $text, $time = 86400)
 {
     $i = 0;
+    $time = time() + $time;
     while (true) {
         $name = 'todo-' . $i;
         $i++;
@@ -9,7 +10,27 @@ function saveData($header, $text, $time = 86400)
             continue;
         }
 
-        setcookie($name, '', $time);
+        $data = array(
+            'header' => $header,
+            'text'   => $text
+        );
+        $encodedData = json_encode($data);
+        setcookie($name, $encodedData, $time);
+        break;
     }
+}
 
+function getData()
+{
+    $data = array();
+    $i = 0;
+    while (true) {
+        $name = 'todo-' . $i;
+        $i++;
+        if (isset($_COOKIE[$name])) {
+            $data[$name] = json_decode($_COOKIE[$name], true);
+        } else {
+            return $data;
+        }
+    }
 }
--- 05.08/todo/index.php
+++ 05.08/todo/index.php
@@ -1,16 +1,22 @@
 <?php
 require 'cookie.php';
 ?>
+<?php
+if (isset($_GET['header']) && $_GET['header'] != '') {
+    saveData($_GET['header'], $_GET['text']);
+}
+?>
 <form action="">
     Заголовок: <input type="text" name="header"> <br />
     Текст: <textarea name="text"></textarea><br />
     <input type="submit">
 </form>
 <?php
-saveData('', '');
+foreach (getData() as $key => $data):
 ?>
-<div class="post">
-    <h1>Заголовок</h1>
-    <img src="" alt="">
-    <p>Текст</p>
-</div>
+    <div class="post">
+        <h1><?= $data['header'] ?></h1>
+        <img width="300px" height="300px" src="https://habrastorage.org/files/bbb/6d0/ce5/bbb6d0ce5b2e409aa9f8fe986d87eeec.png" alt="">
+        <p><?= $data['text'] ?></p>
+    </div>
+<?php endforeach; ?>

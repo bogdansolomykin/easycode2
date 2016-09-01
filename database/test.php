ce91cdc24013c8447806d43c8005e83a1d8093a1
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-09-01 19:33:05 +0300

    * da example

--- database/test.php
+++ database/test.php
@@ -1,7 +1,7 @@
 <?php
 
-$connectionObject = mysqli_connect("localhost", "root", "123", "bogdan"); // создаем соединение с базой данных
-$resource = mysqli_query($connectionObject, "SELECT * FROM author"); // выполянем запрос к базе, указывая при этом какое соединение использовать
+$connectionObject = mysqli_connect("localhost", "root", "123", "bogdan") or die("Ошибка " . mysqli_error($link)); // создаем соединение с базой данных
+$resource = mysqli_query($connectionObject, "SELECT * FROM author") or die("Ошибка запроса" . mysqli_error($link)); // выполянем запрос к базе, указывая при этом какое соединение использовать
 
 $rows = array();
 while (true) {

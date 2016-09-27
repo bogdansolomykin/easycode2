0d26556a084d3ca83e506c1840c325e5a9c7a959
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-09-27 19:43:36 +0300

    * oop method

--- oop/27.09/test.php
+++ oop/27.09/test.php
@@ -2,13 +2,20 @@
 
 class Test
 {
-    public function hello(Human $human) // 1
+    public function testObject(Human $human)
     {
-        echo 'before: ';
-        echo __FUNCTION__ . ':' . $human->name;
+        echo 'before: ' .__FUNCTION__ . ':' . $human->name;
         echo '<br />';
         $human->name = 'Olga';
-        echo __FUNCTION__ . ':' . $human->name;
+        echo 'after: ' . __FUNCTION__ . ':' . $human->name;
+    }
+
+    public function testString($stringParam)
+    {
+        echo 'before: ' .__FUNCTION__ . ':' . $stringParam;
+        echo '<br />';
+        $stringParam = 'Stringnew';
+        echo 'after: ' . __FUNCTION__ . ':' . $stringParam;
     }
 }
 
@@ -18,8 +25,17 @@ class Human
 }
 
 $test = new Test();
-$human = new Human(); // 1
-$human->name = 'Vasya'; // 1
-$test->hello($human);
+
+$human = new Human();
+$human->name = 'Vasya'; // vasya
+$test->testObject($human); // по ссылке
 echo '<br />';
-echo 'GLOBAL:' . $human->name; // 1
+echo 'GLOBAL:' . $human->name; // olga
+
+echo '<br />';
+echo '<br />';
+
+$string = 'String'; // String
+$test->testString($string); // по значению
+echo '<br />';
+echo 'GLOBAL:' . $string; // String

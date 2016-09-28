fc91606fbd32f337f3de7b74cbcc807b05fcd90c
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-09-28 20:01:53 +0300

    * oop

--- oop/27.09/homework/index.php
+++ oop/27.09/homework/index.php
@@ -9,13 +9,14 @@ require_once 'Passenger.php';
 $car = new Car();
 $moto = new Moto();
 $bike = new Bike();
+$tank = new Tank();//
 
 $driver = new Driver();
 $passenger = new Passenger();
 
-$driver->sit($moto);
+$driver->sit($moto); // $tank
 echo '<br />';
-$passenger->sit($moto);
+$passenger->sit($moto); // $tank
 echo '<br />';
 $driver->drive();
 
